fc5f77737afebc1b56379de3793be07d32b1e450
Datcamap <48880635+datcamap@users.noreply.github.com>
2022-03-21 13:11:23 +0700

    adding points when running

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,7 +5,7 @@ struct Ant
 {
 public:
 	int tour[100] = {0};
-	double cost = 0;
+	float cost = 0;
 };
 
 // Override base class with your custom functionality
@@ -17,21 +17,22 @@ public:
 		// Name your application
 		sAppName = "ACO";
 	}
-	int nPoints = 99; // How many points? (no more than 99)
-	int nAnts = 99; // How many ants? (like above)
+	int nPoints = 9; // How many points? (no more than 999)
+	int nAnts = 99; // How many ants? (no more than 9999)
 
-	olc::vd2d Loc[100];
+	olc::vf2d loc[100];
 	Ant bestTour;
-	Ant ant[100];
-	double length[100][100]; // Length of paths (Heuristic information)
-	double pher[100][100]; // Pheromone
-	double alpha = 1;
-	double beta = 1;
-	double rho = 0.1;
+	Ant ant[1000];
+	float length[100][100]; // Length of paths (Heuristic information)
+	float pher[100][100]; // Pheromone
+	float alpha = 1;
+	float beta = 1;
+	float rho = 0.1;
 
 	int fps = 1;
 	int fps2go = 1;
 	int iter = 0;
+	bool added = false;
 
 public:
 	bool OnUserCreate() override
@@ -41,15 +42,15 @@ public:
 		srand((unsigned)time(NULL));
 		for (int i = 1; i <= nPoints; ++i)
 		{
-			Loc[i].x = rand() % ScreenWidth();
-			Loc[i].y = rand() % ScreenHeight();
+			loc[i].x = rand() % ScreenWidth();
+			loc[i].y = rand() % ScreenHeight();
 		}
 		// Setting pheromone state at the start (apparently it was set to 0 but I just do it one more time)
 		for (int i = 1; i <= nPoints; ++i)
 		{
 			for (int j = 1; j <= nPoints; ++j)
 			{
-				pher[i][j] = 0.05;
+				pher[i][j] = 0;
 			}
 		}
 		// Calculate distance between points
@@ -57,7 +58,7 @@ public:
 		{
 			for (int j = i + 1; j <= nPoints; ++j)
 			{
-				length[i][j] = sqrt(pow(Loc[i].x - Loc[j].x, 2) + pow(Loc[i].y - Loc[j].y, 2));
+				length[i][j] = sqrt(pow(loc[i].x - loc[j].x, 2) + pow(loc[i].y - loc[j].y, 2));
 				length[j][i] = length[i][j];
 			}
 		}
@@ -72,8 +73,26 @@ public:
 		// Draw Points
 		for (int p = 1; p <= nPoints; ++p)
 		{
-			DrawCircle(Loc[p], ScreenWidth() / 200 + 2);
-			FillCircle(Loc[p], ScreenWidth() / 200);
+			DrawCircle(loc[p], ScreenWidth() / 200 + 2);
+			FillCircle(loc[p], ScreenWidth() / 200);
+		}
+		if (GetMouse(olc::Mouse::RIGHT).bHeld && !added)
+		{
+			loc[nPoints + 1].x = float(GetMouseX());
+			loc[nPoints + 1].y = float(GetMouseY());
+			++nPoints;
+			int j = nPoints;
+			for (int i = 1; i <= nPoints - 1; ++i)
+			{
+				length[i][j] = sqrt(pow(loc[i].x - loc[j].x, 2) + pow(loc[i].y - loc[j].y, 2));
+				length[j][i] = length[i][j];
+			}
+			added = true;
+			bestTour.cost = INFINITY;
+		}
+		if (GetMouse(olc::Mouse::RIGHT).bReleased && added)
+		{
+			added = false;
 		}
 		// Nested loop to control speed 
 		while (fps > fps2go)
@@ -92,13 +111,13 @@ public:
 				int end = 1;
 				for (int p = 2; p <= nPoints; ++p) // Loop all points
 				{
-					double P[100] = { 0 };
+					float P[100] = { 0 };
 					int i = ant[a].tour[end];
-					double sumP = 0;
+					float sumP = 0;
 					for (int j = 1; j <= nPoints; ++j) // dealing with matrix
 					{
 						// Calculate possibility
-						P[j] = pher[i][j] / length[i][j];
+						P[j] = pher[i][j] / length[i][j] + 0.01;
 						for (int n = 1; n <= end; ++n)
 						{
 							if (j == ant[a].tour[n])
@@ -150,7 +169,7 @@ public:
 				}
 			}
 			++iter;
-			std::cout << " Iteration #" << iter << ": " << bestTour.cost << std::endl;
+			std::cout << " Iteration #" << iter << ": " << bestTour.cost << std::endl << nPoints << std::endl;
 			fps = 0;
 		}
 		++fps;
@@ -159,17 +178,17 @@ public:
 		{
 			int i = bestTour.tour[p];
 			int j = bestTour.tour[p + 1];
-			DrawLineDecal(Loc[i], Loc[j]);
+			DrawLineDecal(loc[i], loc[j]);
 		}
 		return true;
 	}
 
 private:
-	int RouletteWheelSelection(double P[])
+	int RouletteWheelSelection(float P[])
 	{
-		double r = (double)rand()/RAND_MAX;
+		float r = (float)rand()/RAND_MAX;
 		//std::cout << r;
-		double cumsum[100]{0};
+		float cumsum[100]{0};
 		for (int j = 1; j <= nPoints; ++j)
 		{
 			for (int n = 1; n <= j; ++n)
